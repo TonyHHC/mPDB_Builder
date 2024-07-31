f28f6f477bdad6acb547d2afb9432e9aca7b71af
Tony <tonyhhc@gmail.com>
2024-07-31 17:15:28 +0800

    ok

--- builder.py
+++ builder.py
@@ -1,15 +1,43 @@
 import os
 import sys
-import codecs
 import argparse
+import array
 
-def putString(byteArray, tgtString, index):
+############################################################################################
+
+# 將字串編碼為 UTF-16 編碼的數組
+def String2UTF16(tgtString):
+	utf16_encoded = tgtString.encode('utf-16')
+	utf16_encoded_no_bom = utf16_encoded[2:] if utf16_encoded.startswith(b'\xff\xfe') else utf16_encoded
+	
+	return utf16_encoded_no_bom
+
+def UTF162String(tgtArray):
+	# 将數組轉換為 Uint16 數組
+	uint16_array = array.array('H', tgtArray)
+	# 將 Uint16 數組解碼為字串
+	tgtString = uint16_array.tobytes().decode('utf-16')
+	
+	return tgtString
+
+def putStringByUTF16(byteArray, tgtString, index):
+    unicode_bytes = String2UTF16(tgtString)
+    byteArray[index:index+len(unicode_bytes)] = unicode_bytes
+
+def putStringByUTF8(byteArray, tgtString, index):
     unicode_bytes = tgtString.encode('utf-8')
     byteArray[index:index+len(unicode_bytes)] = unicode_bytes
 
 def putInt(byteArray, tgtValue, size, index):
-    headers[index:index+size] = tgtValue.to_bytes(size, 'big')
+    byteArray[index:index+size] = tgtValue.to_bytes(size, 'big')
 
+def getLineFromTXT(txtContent, lineNumber):
+    tmp = UTF162String(txtContent).splitlines()
+    return tmp[lineNumber-1]
+
+############################################################################################
+
+############################################################################################
 # main
 if __name__ == '__main__':
 
@@ -34,9 +62,11 @@ if __name__ == '__main__':
         sys.exit(-1)
 
     iRecords = 0
+    fileLists = []
     for i in range(args.startIndex, args.endIndex+1):
         if os.path.isfile(f'{args.txtSourceDir}\\{i}.txt'):
             iRecords += 1
+            fileLists.append(f'{args.txtSourceDir}\\{i}.txt')
 
     if iRecords <= 0:
         print('Error!! 找不到指定範圍內的 txt 檔')
@@ -50,15 +80,24 @@ if __name__ == '__main__':
             print('Error!! 無法建立輸出的 mPDB 檔目錄')
             sys.exit(-1)
 
+    # 讀取所有 txt 檔
+    txtContents = []
+    for eachFile in fileLists:
+        with open(eachFile, 'r', encoding='utf-8') as f:
+            content = f.read()
+            txtContents.append(String2UTF16(content))
+
+    #print(txtContents)    
+
     # 初始化 Headers
     headers = bytearray(78)
-    putString(headers, args.authorName, 0)
+    putStringByUTF16(headers, args.authorName, 0)
     headers[35] = 2
-    putString(headers, 'BOOK', 60)
-    putString(headers, 'MTIU', 64)
+    putStringByUTF8(headers, 'BOOK', 60)
+    putStringByUTF8(headers, 'MTIU', 64)
     putInt(headers, (iRecords+2), 2, 76)
     
-    print(headers)
+    #print(headers)
 
     '''
     tmp = bytes(headers[0:34+1]).decode('utf-8').strip('\x00')
@@ -69,4 +108,60 @@ if __name__ == '__main__':
     print(tmp)
     '''
     
-    #
+    # 第一筆資料
+    datas_1 = bytearray(b'\x20\x20\x20\x20\x20\x20\x20\x20')
+    datas_1.extend(bytearray(8))
+    datas_1.extend(String2UTF16(args.bookName))
+    datas_1.extend(bytes([27, 0, 27, 0, 27, 0]))
+    datas_1.extend(bytearray(str(iRecords), 'ascii'))
+    datas_1.extend(bytes([27, 0]))
+
+    for txtContent in txtContents:
+        chapterCaption = getLineFromTXT(txtContent, 1).strip()
+        datas_1.extend(String2UTF16(chapterCaption))
+        datas_1.extend(bytes([13, 0, 10, 0]))
+
+    #print(datas)
+
+    # 每筆資料的起始位置及屬性
+    eachDataPos = bytearray((iRecords+2)*8)
+
+    iIndex = 0
+    iLastPos = 78 + ((iRecords+2)*8)
+    putInt(eachDataPos, iLastPos, 4, iIndex)
+
+    iIndex += 8
+    iLastPos += len(datas_1)
+    putInt(eachDataPos, iLastPos, 4, iIndex)
+
+    for txtContent in txtContents:
+        iIndex += 8
+        iLastPos += len(txtContent)
+        putInt(eachDataPos, iLastPos, 4, iIndex)
+    
+    #print(eachDataPos)
+
+    # 第 N+2 筆資料
+    data_N2 = bytearray(b'\xff\xff')
+
+    # 開始建立 UPDB 檔案
+    strOutputPath = args.ouputDir+f'\\{args.bookName}.updb'
+    with open(strOutputPath, 'wb') as updb:
+        # 輸出 header
+        updb.write(headers)
+
+        # 輸出 每筆資料的起始位置及屬性
+        updb.write(eachDataPos)
+
+        # 輸出 第一筆資料
+        updb.write(datas_1)
+
+        # 輸出 N 筆資料
+        for txtContent in txtContents:
+            updb.write(txtContent)
+
+        # 輸出 第 N+2 筆資料
+        updb.write(data_N2)
+
+    # 結束
+    print(f'\n完成建立 {strOutputPath}\n')
